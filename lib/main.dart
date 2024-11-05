d56b95ef2657f43133a5dfd09ba7cd63e5989e02
agsjohn <joaovitormds03@gmail.com>
2024-11-05 16:53:04 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,274 @@
+import 'dart:convert';
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      // home: const FirstRoute(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+Future<List<dynamic>> fetchUsers() async {
+  final url = Uri.parse('https://ca8bf909bc8e54bd6dff.free.beeceptor.com/api/users');
+  // final url = Uri.parse('https://crudcrud.com/api/d978b2feffe74264baf3fa98dee5dd84');
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    print('Resposta: ${response.body}');
+    return jsonDecode(response.body);
+  } else {
+    print('Erro: ${response.statusCode}');
+    {
+      throw Exception('Failed to load data');
+    }
+  }
+}
+
+Future<void> createData(String nome, String sobrenome, String genero, int idade, String email) async {
+  final response = await http.post(Uri.parse('https://ca8bf909bc8e54bd6dff.free.beeceptor.com/api/users/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'nome': nome,
+        'sobrenome': sobrenome,
+        'genero': genero,
+        'idade': idade,
+        'email': email
+      }));
+  if (response.statusCode != 200) {
+    throw Exception('Failed to create data');
+  }
+}
+
+Future<void> deleteData(String id) async {
+  final response = await http.delete(Uri.parse('https://ca8bf909bc8e54bd6dff.free.beeceptor.com/api/users/$id'));
+
+  if (response.statusCode != 200) {
+    throw Exception('Failed to delete data');
+  }
+}
+
+Future<void> updateData(int id) async {
+  final response = await http.put(Uri.parse('https://jsonplaceholder.typicode.com/posts/$id'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'title': 'Flutter HTTP CRUD',
+        'body': 'This is an updated blog post about HTTP CRUD methods in Flutter',
+        'userId': 1,
+      }));
+
+  if (response.statusCode != 200) {
+    throw Exception('Failed to update data');
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String nome="";
+  String sobrenome="";
+  String genero="";
+  int idade=0;
+  String email="";
+
+  // final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Dados utilizando requisições HTTP'),
+        ),
+        body: Column(
+          children: [
+            Expanded(child: FutureBuilder<List<dynamic>>(
+              future: fetchUsers(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Erro: ${snapshot.error}'));
+                } else {
+                  final data = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      sobrenome = data[index]['sobrenome'];
+                      genero = data[index]['genero'];
+                      idade = data[index]['idade'];
+                      email = data[index]['email'];
+                      return ListTile(
+                        title: Text(data[index]['nome']),
+                        subtitle: Text(
+                            'Sobrenome: $sobrenome \nGênero: $genero\nIdade: $idade\nEmail: $email'),
+                      );
+                    },
+                  );
+                }
+              },
+            ),),
+            ElevatedButton(onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SegundaJanela(title: 'Flutter Demo')),
+              );
+            }, child: const Text("Adicionar usuário")),
+          ]
+        ),
+    );
+  }
+}
+
+class SegundaJanela extends StatefulWidget {
+  const SegundaJanela({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<SegundaJanela> createState() => SecondRoute();
+}
+
+class SecondRoute extends State<SegundaJanela> {
+
+  final _formKey = GlobalKey<FormState>();
+  
+  String nome="";
+  String sobrenome="";
+  String genero="";
+  int idade=0;
+  String email="";
+
+  int groupRadio = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Scaffold(
+      appBar: null,
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(10),
+              child:
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, digite um nome';
+                  }
+                  return null;
+                },
+                onChanged: (value) => nome=value,
+                decoration: const InputDecoration(
+                labelText: 'Digite seu nome')
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child:
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, digite um sobrenome';
+                  }
+                  return null;
+                },
+                onChanged: (value) => sobrenome=value,
+                decoration: const InputDecoration(
+                labelText: 'Digite seu sobrenome')
+              ),
+            ),
+            RadioListTile(title: const Text("Feminino"),value: 1,
+              groupValue: groupRadio, onChanged: (int? value){setState((){
+                genero = "Feminino";
+                groupRadio = value!;
+            });}),
+            RadioListTile(title: const Text("Masculino"),value: 2,
+              groupValue: groupRadio, onChanged: (int? value){setState((){
+                genero = "Masculino";
+                groupRadio = value!;
+            });}),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child:
+              TextFormField(
+                inputFormatters: <TextInputFormatter>[FilteringTextInputFormatter.digitsOnly],
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, digite uma idade';
+                  }
+                  return null;
+                },
+                onChanged: (value) => idade = int.tryParse(value) ?? 0,
+                decoration: const InputDecoration(
+                labelText: 'Digite sua idade')
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child:
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, digite um email';
+                  }
+                  return null;
+                },
+                onChanged: (value) => email=value,
+                decoration: const InputDecoration(
+                labelText: 'Digite seu email')
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: 
+              ElevatedButton(
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false otherwise.
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, display a snackbar. In the real world,
+                    // you'd often call a server or save the information in a database.
+                    createData(nome, sobrenome, genero, idade, email);
+                    Navigator.pop(context);
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ),
+          ]
+        ),
+      )
+    );
+  }
+}
